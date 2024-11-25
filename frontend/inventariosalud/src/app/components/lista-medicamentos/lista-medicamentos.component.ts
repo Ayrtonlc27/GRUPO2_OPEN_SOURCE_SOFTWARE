19a4b4a5aeefcdb9ded37bbfeecd3c79d7f4d813
Melchor Luis Criollo Medrano <mlcriollo@indracompany.com>
2024-11-24 23:37:48 -0500

    Exportar pdf

--- frontend/inventariosalud/src/app/components/lista-medicamentos/lista-medicamentos.component.ts
+++ frontend/inventariosalud/src/app/components/lista-medicamentos/lista-medicamentos.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { MedicamentoService,  } from '../../services/medicamento.service';
 import { Medicamento } from '../../models/Medicamento';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
+import { jsPDF } from 'jspdf';
+import 'jspdf-autotable';
 
 @Component({
   selector: 'app-lista-medicamentos',
   templateUrl: './lista-medicamentos.component.html'
 })
 export class ListaMedicamentosComponent implements OnInit {
+
   medicamentos: Medicamento[] = [];
   medicamentoSeleccionado:  Medicamento = {
     nombre: '',
@@ -86,6 +89,34 @@ export class ListaMedicamentosComponent implements OnInit {
       cantidadDisponible: 0,
       precio: 0
     };; // Limpia el medicamento seleccionado
+  }
+
+  exportarPDF() {
+    const doc = new jsPDF();
+
+    // Agregar título
+    doc.text('Lista de Medicamentos', 10, 10);
+
+    // Generar datos para la tabla
+    const datosTabla = this.medicamentos.map(m => [
+      m.nombre,
+      m.categoria,
+      m.codigo,
+      m.nivelMinimo,
+      m.fechaExpiracion, // Formato de fecha
+      m.cantidadDisponible,
+      m.precio.toFixed(2)
+    ]);
+
+    // Crear tabla
+    (doc as any).autoTable({
+      head: [['Nombre', 'Categoría', 'Código', 'Nivel Mínimo', 'Fecha Expiración', 'Cantidad', 'Precio']],
+      body: datosTabla,
+      startY: 20
+    });
+
+    // Descargar el archivo
+    doc.save('lista-medicamentos.pdf');
   }
   
 }
